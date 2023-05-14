7a7b4900b8f1eff8ccc87f34431c2b08ccdb568f
smartbear <wqeqsada2131@gmail.com>
2023-05-14 10:48:35 -0500

    finish split link and word

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import (
     redirect,
     url_for,
     send_from_directory,
+    session,
 )
 from werkzeug.utils import secure_filename
 from backend import process_link as pl
@@ -94,7 +95,6 @@ def allowed_file(filename):
 
 @app.route("/", methods=["GET", "POST"])
 def link_to_text():
-    finish = False
     if request.method == "POST":
         linktxt = request.files["linktxt"]
         linkarea = request.form["linkarea"]
@@ -112,26 +112,91 @@ def link_to_text():
             )
         else:
             link = [i.replace("\xa0", " ") for i in linkarea.split("\n") if i != ""]
+            print(link)
 
         file_lk_map, fail_map = pl.write_to_files(link, timeout=stimeout)
+        session["file_lk_map"] = file_lk_map
+        session["fail_map"] = fail_map
+        session["link"] = link
         utils.zip_files()
-        filename = "link_result.zip"
-        print("finish parse", file_lk_map)
-        finish = True
-        return render_template("link.html", finish=finish, filename=filename)
-
-    return render_template("link.html", finish=finish)
+        session["filename"] = {"link": "link_result.zip"}
+        session["source"] = "link"
+        return redirect("/result")
+    return render_template("link.html")
 
 
-@app.route("/uploads/<filename>")
-def uploaded_file(filename):
-    print(os.path.join("result", filename))
-    return send_from_directory("./result", filename)
-
-
-@app.route("/search_word")
+@app.route("/search_word", methods=["GET", "POST"])
 def search_word():
-    return "hello world"
+    print(request.method)
+    if request.method == "POST":
+        print(request.files, request.form)
+        wordtxt = request.files["wordtxt"]
+        wordarea = request.form["wordarea"]
+        if wordtxt and allowed_file(wordtxt.filename):
+            filename = secure_filename(wordtxt.filename)
+            wordtxt.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            word = pw.get_word_from_path(
+                os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            )
+        else:
+            word = list(
+                set(
+                    [
+                        i.replace("\xa0", " ").replace("\u202f", " ")
+                        for i in wordarea.split("\n")
+                    ]
+                )
+            )
+        case = True if request.form["case"] == "True" else False
+        exact = True if request.form["exact"] == "True" else False
+        above = int(request.form["above"])
+        below = int(request.form["below"])
+
+        file_lk_map = session["file_lk_map"]
+        fail_map = session["fail_map"]
+        link = session["link"]
+        final, lk_no_hit = pw.run_search(
+            file_lk_map,
+            word,
+            above=above,
+            below=below,
+            ignore_case=case,
+            exact_match=exact,
+        )
+
+        df = pd.DataFrame(final, columns=["link", "word", "content"])
+        utils.df_to_xlsx(df, "./result/result.xlsx")
+
+        summary = []
+        for l in link:
+            if l in fail_map:
+                summary.append([l, fail_map[l]])
+            elif l in lk_no_hit:
+                summary.append([l, "no hit"])
+            else:
+                summary.append([l, "hit"])
+        summary = pd.DataFrame(summary, columns=["Link", "Result"])
+        with pd.ExcelWriter(
+            "./result/result.xlsx", engine="openpyxl", mode="a"
+        ) as writer:
+            summary.to_excel(writer, sheet_name="summary")
+
+        session["filename"].update({"result": "result.xlsx"})
+        session["source"] = "word"
+        return redirect("/result")
+    return render_template("word.html")
+
+
+@app.route("/result")
+def result_page():
+    return render_template(
+        "result.html", filename=session["filename"], source=session["source"]
+    )
+
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory("./result", filename)
 
 
 if __name__ == "__main__":
